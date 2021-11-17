1aee7bf5c1c0b614da0bc5b20e3c68d370c4a023
chatreejs <chanon29060@live.com>
2021-11-17 17:25:44 +0700

    feat: Add database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
@@ -6,7 +7,29 @@ import { AppService } from './app.service';
 import { FoodModule } from './food/food.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), FoodModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        '.development.local.env',
+        '.development.env',
+        '.env',
+        '.production.env',
+      ],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      options: {
+        encrypt: false,
+      },
+      synchronize: process.env.NODE_ENV === 'development',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    FoodModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
